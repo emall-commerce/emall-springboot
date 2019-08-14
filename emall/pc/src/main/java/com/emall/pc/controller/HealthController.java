88d9fa1bc5abfe7a4ca516730331010b9915fa75
xiaobin <xiaobin@lxrental.com>
2019-08-14 15:26:15 +0800

    提交consul接入健康检查，修改readme

---
+++ emall/pc/src/main/java/com/emall/pc/controller/HealthController.java
@@ -0,0 +1,21 @@
+package com.emall.pc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <consul health 检查>
+ * 健康检查
+ * @author win7
+ *
+ */ 
+@RestController
+@RequestMapping(value = "/") 
+public class HealthController {
+	 @GetMapping("/health")
+	    public String health(){
+	        return "health"; 
+   }
+ 
+}
