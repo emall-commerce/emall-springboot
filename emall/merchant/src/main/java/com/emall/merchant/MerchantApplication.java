86d3458e9c0b4c76c8910156ab53c3e897cfc8c9
xiaobin <xiaobin@lxrental.com>
2019-08-14 11:58:30 +0800

    修改readme 新增商户模块

---
+++ emall/merchant/src/main/java/com/emall/merchant/MerchantApplication.java
@@ -0,0 +1,15 @@
+package com.emall.wap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * <商户信息工程--暂不启用>
+ */
+@SpringBootApplication
+public class MerchantApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantApplication.class, args);
+    }
+}
---
+++ emall/thirdpart/src/main/java/com/emall/thirdpart/ThirdpartApplication.java
@@ -0,0 +1,15 @@
+package com.emall.pc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * <PC端交互使用--冗余>
+ */
+@SpringBootApplication
+public class ThirdpartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdpartApplication.class, args);
+    }
+}
