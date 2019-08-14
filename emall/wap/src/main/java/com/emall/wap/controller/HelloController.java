eab0d85e30f90d440ef64822f113c1a737abdec2
xiaobin <xiaobin@lxrental.com>
2019-08-14 16:10:45 +0800

    添加日志

--- emall/wap/src/main/java/com/emall/wap/controller/HelloController.java
+++ emall/wap/src/main/java/com/emall/wap/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.emall.wap.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,11 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
  * @Desc: 这是一个测试类
  */
 @RestController
-@RequestMapping ("hello")
+@RequestMapping("hello")
 public class HelloController {
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @GetMapping("/say")
-    public String sayHelloWorld(@RequestParam String name){
-        return  "hello " + name;
+    public String sayHelloWorld(@RequestParam String name) {
+        logger.info("测试日志【{}】", name);
+        logger.error("测试错误日志【{}】", name);
+        return "hello " + name;
     }
 }
